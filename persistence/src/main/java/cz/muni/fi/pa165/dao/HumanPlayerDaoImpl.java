e97d89101e90f2bc7d6d6b370a3659c3d927182e
Martin Barnáš <xbarnas@fi.muni.cz>
2017-10-20 03:36:08 +0200

    NF add HumanPlayerDao implementation
    #7

---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/HumanPlayerDaoImpl.java
@@ -0,0 +1,53 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.HumanPlayer;
+import cz.muni.fi.pa165.repositories.HumanPlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Martin Barnas 433523@mail.muni.cz
+ */
+@Repository
+public class HumanPlayerDaoImpl implements HumanPlayerDao {
+
+  @Autowired
+  private HumanPlayerRepository humanPlayerRepository;
+
+  @Override
+  public void create(HumanPlayer humanPlayer) {
+    humanPlayerRepository.save(humanPlayer);
+  }
+
+  @Override
+  public HumanPlayer update(HumanPlayer humanPlayer) {
+    return humanPlayerRepository.save(humanPlayer);
+  }
+
+  @Override
+  public void delete(HumanPlayer humanPlayer) {
+    humanPlayerRepository.delete(humanPlayer);
+  }
+
+  @Override
+  public HumanPlayer findById(Long id) {
+    return humanPlayerRepository.findById(id);
+  }
+
+  @Override
+  public HumanPlayer findByEmail(String email) {
+    return humanPlayerRepository.findByEmail(email);
+  }
+
+  @Override
+  public HumanPlayer findByUsername(String username) {
+    return humanPlayerRepository.findByUsernameIgnoreCase(username);
+  }
+
+  @Override
+  public List<HumanPlayer> findAll() {
+    return humanPlayerRepository.findAll();
+  }
+}
