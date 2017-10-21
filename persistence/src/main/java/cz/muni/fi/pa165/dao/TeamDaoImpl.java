56e8487dd7d41c1c5d24c0ba7f31118d11984a7a
Hathoriel <lukas.kotol@gmail.com>
2017-10-21 16:51:16 +0200

    NF Created Team entity and Dao.

---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/TeamDaoImpl.java
@@ -0,0 +1,51 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Team;
+import cz.muni.fi.pa165.enums.CompetitionCountry;
+import cz.muni.fi.pa165.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TeamDaoImpl implements TeamDao {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Override
+    public void create(Team team) {
+        teamRepository.save(team);
+    }
+
+    @Override
+    public Team update(Team team) {
+        return teamRepository.save(team);
+    }
+
+    @Override
+    public void delete(Team team) {
+        teamRepository.delete(team);
+    }
+
+    @Override
+    public Team findById(Long id) {
+        return teamRepository.findById(id);
+    }
+
+    @Override
+    public Team findByName(String name) {
+        return teamRepository.findByName(name);
+    }
+
+    @Override
+    public List<Team> findByCompetitionCountry(CompetitionCountry competitionCountry) {
+        return teamRepository.findByCompetitionCountry(competitionCountry);
+    }
+
+    @Override
+    public List<Team> finAll() {
+        return teamRepository.finAll();
+    }
+}
