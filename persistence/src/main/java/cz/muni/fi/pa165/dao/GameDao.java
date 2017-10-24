521e787eeb51ce25047740d8036ba9748db93fce
Markéta Elederová <elederovam@seznam.cz>
2017-10-24 22:52:21 +0200

    NF add Game dao

---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/GameDao.java
@@ -0,0 +1,68 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Game;
+import cz.muni.fi.pa165.entity.Team;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ *
+ * @author Marketa Elederova
+ */
+public interface GameDao {
+
+    /**
+    * Create {@link Game}.
+    *
+    * @param game to create
+    */
+    void create(Game game);
+
+    /**
+    * Update {@link Game}.
+    *
+    * @param game to update
+    * @return updated game
+    */
+    Game update(Game game);
+
+    /**
+    * Delete {@link Game}.
+    *
+    * @param game to delete
+    */
+    void delete(Game game);
+
+    /**
+    * Find {@link Game} by id.
+    *
+    * @param id to find
+    * @return game with given id
+    */
+    Game findById(Long id);
+
+    /**
+     * Find {@link Game}s by first team, second team and start time.
+     *
+     * @param firstTeam
+     * @param secondTeam
+     * @param startTime
+     * @return all games with given first team, second team and start time
+     */
+    Game findByFirstTeamAndSecondTeamAndStartTime(Team firstTeam, Team secondTeam, LocalDateTime startTime);
+
+    /**
+     * Find {@link Game}s by any team.
+     *
+     * @param team
+     * @return all games of given team
+     */
+    List<Game> findByTeam(Team team);
+
+    /**
+    * Find all {@link Game}s.
+    *
+    * @return all games
+    */
+    List<Game> findAll();
+}
---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/GameDaoImpl.java
@@ -0,0 +1,53 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Game;
+import cz.muni.fi.pa165.entity.Team;
+import cz.muni.fi.pa165.repositories.GameRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Marketa Elederova
+ */
+public class GameDaoImpl implements GameDao {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Override
+    public void create(Game game) {
+        gameRepository.save(game);
+    }
+
+    @Override
+    public Game update(Game game) {
+        return gameRepository.save(game);
+    }
+
+    @Override
+    public void delete(Game game) {
+        gameRepository.delete(game);
+    }
+
+    @Override
+    public Game findById(Long id) {
+        return gameRepository.findById(id);
+    }
+
+    @Override
+    public Game findByFirstTeamAndSecondTeamAndStartTime(Team firstTeam, Team secondTeam, LocalDateTime startTime) {
+        return gameRepository.findByFirstTeamAndSecondTeamAndStartTime(firstTeam, secondTeam, startTime);
+    }
+
+    @Override
+    public List<Game> findByTeam(Team team) {
+        return gameRepository.findByFirstTeamOrSecondTeam(team, team);
+    }
+
+    @Override
+    public List<Game> findAll() {
+        return gameRepository.findAll();
+    }
+}
