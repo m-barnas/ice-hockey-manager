093c34977592b6e0b4c199a3ac5a7306f805c63e
Hathoriel <lukas.kotol@gmail.com>
2017-11-23 21:55:55 +0100

    NF tests updated and TeamFacade created.

--- persistence/src/main/java/cz/muni/fi/pa165/facades/TeamFacade.java
+++ persistence/src/main/java/cz/muni/fi/pa165/facades/TeamFacade.java
@@ -1,7 +1,13 @@
 package cz.muni.fi.pa165.facades;
 
+import cz.muni.fi.pa165.dto.TeamCreateDTO;
+
 /**
  * @author Lukáš Kotol
  */
 public interface TeamFacade {
+    public Long createTeam(TeamCreateDTO teamCreateDTO);
+    public void addHockyPlayer(Long teamId, Long hockeyPlayerId);
+    public void removeHockeyPlayer(Long teamId, Long hockeyPlayerId);
+    public void deleteTeam(Long teamId);
 }
