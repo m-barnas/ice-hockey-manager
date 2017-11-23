c0ae3d4d6dc2bfe21aaaac9637647d0abaa1c7fa
Martin Barnáš <xbarnas@fi.muni.cz>
2017-11-23 02:45:58 +0100

    NF add BeanMappingServiceImpl
    #31

---
+++ ice-hockey-manager-service/src/main/java/cz/muni/fi/pa165/service/mappers/BeanMappingServiceImpl.java
@@ -0,0 +1,39 @@
+package cz.muni.fi.pa165.service.mappers;
+
+import cz.muni.fi.pa165.service.mappers.BeanMappingService;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Bean mapping service implementation.
+ *
+ * @author Martin Barnas 433523@mail.muni.cz
+ */
+public class BeanMappingServiceImpl implements BeanMappingService {
+
+    @Autowired
+    private Mapper dozer;
+
+    @Override
+    public <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass) {
+        List<T> mappedCollection = new ArrayList<>();
+        for (Object object : objects) {
+            mappedCollection.add(dozer.map(object, mapToClass));
+        }
+        return mappedCollection;
+    }
+
+    @Override
+    public <T> T mapTo(Object u, Class<T> mapToClass) {
+        return dozer.map(u,mapToClass);
+    }
+
+    @Override
+    public Mapper getMapper() {
+        return dozer;
+    }
+}
