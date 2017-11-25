312588537748b9d62e55c2a87d25d3ca0c8d360e
H269193 <lukas.kotol@cleverlance.com>
2017-11-25 12:44:42 +0100

    NF TeamService added.

---
+++ ice-hockey-manager-service/src/main/java/cz/muni/fi/pa165/service/TeamService.java
@@ -0,0 +1,30 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.entity.HockeyPlayer;
+import cz.muni.fi.pa165.entity.Team;
+import cz.muni.fi.pa165.enums.CompetitionCountry;
+import cz.muni.fi.pa165.service.exceptions.TeamServiceException;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by Lukas Kotol on 17.11.17.
+ */
+
+@Service
+public interface TeamService {
+    public Team findById(Long id);
+    public List<Team> findAll();
+    public List<Team> findByCompetitionCountry(CompetitionCountry competitionCountry);
+    public Team findByName(String name);
+    public Team createTeam(Team team);
+    public void deleteTeam(Team team);
+    public void addHockeyPlayer(Team team, HockeyPlayer hockeyPlayer);
+    public void removeHockeyPlayer(Team team, HockeyPlayer hockeyPlayer);
+    public void spendMoneyFromBudget(Team team, BigDecimal amount) throws TeamServiceException;
+    public BigDecimal getTeamPrice(Team team);
+    public int getTeamAttackSkill(Team team);
+    public int getTeamDefenseSkill(Team team);
+}
