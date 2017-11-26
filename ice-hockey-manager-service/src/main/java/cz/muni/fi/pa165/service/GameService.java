613ea397824b1404c2d3b944df9a6a308d857d88
Markéta Elederová <445491@mail.muni.cz>
2017-11-26 17:42:53 +0100

    NF add GameService

---
+++ ice-hockey-manager-service/src/main/java/cz/muni/fi/pa165/service/GameService.java
@@ -0,0 +1,76 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.entity.Game;
+import cz.muni.fi.pa165.entity.Team;
+
+import java.util.List;
+
+/**
+ * Class for managing games.
+ *
+ * @author Marketa Elederova
+ */
+public interface GameService {
+
+    /**
+     * Create new {@link Game}.
+     *
+     * @param game to create
+     */
+    void create(Game game);
+
+    /**
+     * Delete {@link Game} from system.
+     *
+     * @param game to delete
+     */
+    void delete(Game game);
+
+    /**
+     * Update {@link Game}.
+     *
+     * @param game to update
+     * @return updated game
+     * @throws IllegalArgumentException if (new) game has been already played
+     * (has not null score)
+     */
+    Game update(Game game);
+
+    /**
+     * Find {@link Game} by id.
+     *
+     * @param id game id
+     * @return Game with given id
+     */
+    Game findById(Long id);
+
+    /**
+     * Find {@link Game}s of team.
+     *
+     * @param team playing in game
+     * @return list of all games of given team
+     */
+    List<Game> findByTeam(Team team);
+
+    /**
+    * Find all {@link Game}s.
+    *
+    * @return list of all games
+    */
+    List<Game> findAll();
+
+    /**
+     * Find all games with state OK which were not played yet (have null score).
+     *
+     * @return list of all scheduled games
+     */
+    List<Game> findScheduledGames();
+
+    /**
+     * Play all not played hockey games in the past
+     * and set its score for both teams.
+     *
+     * @return list of just playd games (with not null scores)
+     */
+    List<Game> playGames();
+}
---
+++ ice-hockey-manager-service/src/main/java/cz/muni/fi/pa165/service/GameServiceImpl.java
@@ -0,0 +1,166 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.dao.GameDao;
+import cz.muni.fi.pa165.entity.Game;
+import cz.muni.fi.pa165.entity.HockeyPlayer;
+import cz.muni.fi.pa165.entity.Team;
+import cz.muni.fi.pa165.enums.GameState;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation of {@link GameService} interface
+ *
+ * @author Marketa Elederova
+ */
+@Service
+public class GameServiceImpl implements GameService {
+
+    @Autowired
+    private GameDao gameDao;
+
+    @Override
+    public void create(Game game) {
+        gameDao.create(game);
+    }
+
+    @Override
+    public void delete(Game game) {
+        gameDao.delete(game);
+    }
+
+    @Override
+    public Game update(Game game) {
+        if ((game.getFirstTeamScore() != null) || (game.getSecondTeamScore() != null)) {
+            throw new IllegalArgumentException("Game has been already played.");
+        }
+        return gameDao.update(game);
+    }
+
+    @Override
+    public Game findById(Long id) {
+        return gameDao.findById(id);
+    }
+
+    @Override
+    public List<Game> findByTeam(Team team) {
+        return gameDao.findByTeam(team);
+    }
+
+    @Override
+    public List<Game> findAll() {
+        return gameDao.findAll();
+    }
+
+    @Override
+    public List<Game> findScheduledGames() {
+        return gameDao.findScheduledGames();
+    }
+
+    @Override
+    public List<Game> playGames() {
+        List<Game> games = findScheduledGames();
+        Predicate<Game> predicateGamesInFuture =
+                game -> game.getStartTime().isAfter(LocalDateTime.now());
+        games.removeIf(predicateGamesInFuture);
+
+        for (Game game : games) {
+            playGame(game);
+        }
+
+        return games;
+    }
+
+    /**
+     * Play game and set score of both teams.
+     *
+     * @param game to play
+     * @throws IllegalArgumentException if game is canceled or if game start
+     * time is in future or game has been already played (has not null score)
+     */
+    public void playGame(Game game) {
+        if (game.getGameState().equals(GameState.CANCELED)) {
+            throw new IllegalArgumentException("Game is canceled.");
+        }
+        if ((game.getFirstTeamScore() != null) || (game.getSecondTeamScore() != null)) {
+            throw new IllegalArgumentException("Game has been already played.");
+        }
+        if (game.getStartTime().isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Game start time is in future.");
+        }
+
+        List<HockeyPlayer> firstTeamPlayers = new ArrayList(game.getFirstTeam().getHockeyPlayers());
+        List<HockeyPlayer> secondTeamPlayers = new ArrayList(game.getSecondTeam().getHockeyPlayers());
+        int firstTeamScore = countScore(countAverageAttackSkill(firstTeamPlayers), countAverageDefenseSkill(secondTeamPlayers));
+        int secondTeamScore = countScore(countAverageAttackSkill(secondTeamPlayers), countAverageDefenseSkill(firstTeamPlayers));
+        game.setFirstTeamScore(firstTeamScore);
+        game.setSecondTeamScore(secondTeamScore);
+    }
+
+    private double countAverageAttackSkill(List<HockeyPlayer> players) {
+        List<Integer> attackSkills = new ArrayList<>();
+        for (HockeyPlayer player : players) {
+            attackSkills.add(player.getAttackSkill());
+        }
+
+        return countAverageSkill(attackSkills);
+    }
+
+    private double countAverageDefenseSkill(List<HockeyPlayer> players) {
+        List<Integer> defenseSkills = new ArrayList<>();
+        for (HockeyPlayer player : players) {
+            defenseSkills.add(player.getDefenseSkill());
+        }
+
+        return countAverageSkill(defenseSkills);
+    }
+
+    /**
+     * Count average attack skill.
+     * If is more than 22 players in the team, the 22 best is chosen.
+     * If there is less then 6 players, new "virtual" players with skill 1 are
+     * added.
+     *
+     * @param skills
+     * @return average skill of players in team
+     */
+    private double countAverageSkill(List<Integer> skills) {
+        if (skills.size() > 22) {
+            skills.sort((Integer i1, Integer i2) -> i2.compareTo(i1));
+            skills = skills.subList(0, 22);
+        }
+        while (skills.size() < 6) {
+            skills.add(1);
+        }
+
+        Integer sum = 0;
+        for (Integer num : skills) {
+            sum += num;
+        }
+        return sum.doubleValue() / skills.size();
+    }
+
+    /**
+     * Magic algorithm to count score of team.
+     *
+     * @param attackSkill
+     * @param defenseSkill
+     * @return
+     */
+    int countScore(double attackSkill, double opponentDefenseSkill) {
+        Random random = new Random();
+        double percent1 = random.nextInt(25);
+        double percent2 = random.nextInt(25);
+        double fortune = random.nextInt(101);
+        return (int) Math.round(
+                Math.pow(
+                        (attackSkill + attackSkill * percent1 / 100) /
+                (opponentDefenseSkill + opponentDefenseSkill * percent2 / 100), 2)
+                + (fortune / 100 - 0.5));
+    }
+}
