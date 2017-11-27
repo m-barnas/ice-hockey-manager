a4eb11862a155f4896363d0c6622a8e7efcaa420
Markéta Elederová <445491@mail.muni.cz>
2017-11-27 14:39:50 +0100

    NF add GameFacadeTest

--- ice-hockey-manager-api/src/main/java/cz/muni/fi/pa165/dto/GameDto.java
+++ ice-hockey-manager-api/src/main/java/cz/muni/fi/pa165/dto/GameDto.java
@@ -23,4 +23,60 @@ public class GameDto {
     private Integer secondTeamScore;
 
     private GameState gameState;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TeamDTO getFirstTeamDto() {
+        return firstTeamDto;
+    }
+
+    public void setFirstTeamDto(TeamDTO firstTeamDto) {
+        this.firstTeamDto = firstTeamDto;
+    }
+
+    public TeamDTO getSecondTeamDto() {
+        return secondTeamDto;
+    }
+
+    public void setSecondTeamDto(TeamDTO secondTeamDto) {
+        this.secondTeamDto = secondTeamDto;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public Integer getFirstTeamScore() {
+        return firstTeamScore;
+    }
+
+    public void setFirstTeamScore(Integer firstTeamScore) {
+        this.firstTeamScore = firstTeamScore;
+    }
+
+    public Integer getSecondTeamScore() {
+        return secondTeamScore;
+    }
+
+    public void setSecondTeamScore(Integer secondTeamScore) {
+        this.secondTeamScore = secondTeamScore;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
 }
