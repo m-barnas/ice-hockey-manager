382cf36305b182f263b921139743f0d47ee03726
Markéta Elederová <445491@mail.muni.cz>
2017-11-27 15:49:40 +0100

    NF add more tests in GameServiceTest

--- ice-hockey-manager-service/src/test/java/cz/muni/fi/pa165/service/GameServiceTest.java
+++ ice-hockey-manager-service/src/test/java/cz/muni/fi/pa165/service/GameServiceTest.java
@@ -15,15 +15,20 @@ import java.util.List;
 import java.util.Set;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.mockito.Matchers;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for GameService class.
@@ -46,6 +51,10 @@ public class GameServiceTest extends AbstractTestNGSpringContextTests {
     private Team team1;
 
     private Team team2;
+
+    private Game gameNow;
+
+    private Game gameInFuture;
 
     @BeforeClass
     public void setUp() {
@@ -76,24 +85,92 @@ public class GameServiceTest extends AbstractTestNGSpringContextTests {
         team2.setHockeyPlayers(players2);
     }
 
+    @BeforeMethod
+    public void initGames() {
+        gameNow = createGame(LocalDateTime.now(clock), false, GameState.OK);
+        gameInFuture = createGame(LocalDateTime.of(2017, 10, 5, 5, 5), false, GameState.OK);
+    }
+
+    @Test
+    public void create() {
+        doAnswer((Answer<Object>) (InvocationOnMock invocationOnMock) -> {
+            Object[] args = invocationOnMock.getArguments();
+            Game createdGame = (Game) args[0];
+            assertThat(createdGame).isEqualTo(gameNow);
+            return null;
+        }).when(gameDao).create(Matchers.any(Game.class));
+
+        gameService.create(gameNow);
+    }
+
+    @Test
+    public void delete() {
+        doAnswer((Answer<Object>) (InvocationOnMock invocationOnMock) -> {
+            Object[] args = invocationOnMock.getArguments();
+            Game createdGame = (Game) args[0];
+            assertThat(createdGame).isEqualTo(gameNow);
+            return null;
+        }).when(gameDao).delete(Matchers.any(Game.class));
+
+        gameService.delete(gameNow);
+    }
+
+    @Test
+    public void update() {
+        doAnswer((Answer<Object>) (InvocationOnMock invocationOnMock) -> {
+            Object[] args = invocationOnMock.getArguments();
+            Game createdGame = (Game) args[0];
+            assertThat(createdGame).isEqualTo(gameNow);
+            return null;
+        }).when(gameDao).update(Matchers.any(Game.class));
+
+        gameService.update(gameNow);
+    }
+
+    @Test
+    public void findById() {
+        when(gameDao.findById(1l)).thenReturn(gameNow);
+        Game game = gameService.findById(1l);
+        assertThat(game).isEqualTo(gameNow);
+    }
+
+    @Test
+    public void findByTeam() {
+        List<Game> games = new ArrayList<>();
+        games.add(gameNow);
+        games.add(gameInFuture);
+        when(gameDao.findByTeam(team1)).thenReturn(games);
+
+        List<Game> foundGames = gameService.findByTeam(team1);
+        assertThat(foundGames).containsExactlyInAnyOrder(gameNow, gameInFuture);
+    }
+
+    @Test
+    public void findAll() {
+        List<Game> games = new ArrayList<>();
+        games.add(gameNow);
+        games.add(gameInFuture);
+        when(gameDao.findAll()).thenReturn(games);
+
+        List<Game> foundGames = gameService.findAll();
+        assertThat(foundGames).containsExactlyInAnyOrder(gameNow, gameInFuture);
+    }
+
     @Test
     public void findScheduledGames() {
-        List<Game> scheduledGames = new ArrayList<>();
-        Game gameInPast = createGame(LocalDateTime.of(2017, 5, 5, 5, 5), false, GameState.OK);
-        Game gameInFuture = createGame(LocalDateTime.of(2017, 10, 5, 5, 5), false, GameState.OK);
-        scheduledGames.add(gameInPast);
-        scheduledGames.add(gameInFuture);
-        when(gameDao.findScheduledGames()).thenReturn(scheduledGames);
+        List<Game> games = new ArrayList<>();
+        games.add(gameNow);
+        games.add(gameInFuture);
+        when(gameDao.findScheduledGames()).thenReturn(games);
 
-        assertThat(gameService.findScheduledGames()).containsExactlyInAnyOrder(gameInPast, gameInFuture);
+        List<Game> foundGames = gameService.findScheduledGames();
+        assertThat(foundGames).containsExactlyInAnyOrder(gameNow, gameInFuture);
     }
 
     @Test
     public void playGames() {
         List<Game> scheduledGames = new ArrayList<>();
         Game gameInPast = createGame(LocalDateTime.of(2017, 5, 5, 5, 5), false, GameState.OK);
-        Game gameInFuture = createGame(LocalDateTime.of(2017, 10, 5, 5, 5), false, GameState.OK);
-        Game gameNow = createGame(LocalDateTime.now(clock), false, GameState.OK);
         scheduledGames.add(gameInPast);
         scheduledGames.add(gameInFuture);
         scheduledGames.add(gameNow);
@@ -115,6 +192,53 @@ public class GameServiceTest extends AbstractTestNGSpringContextTests {
     public void playGameWithNullGame() {
         Game game = null;
         assertThatThrownBy(() -> gameService.playGame(game)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameWithNullGameState() {
+        gameNow.setGameState(null);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameWithNullStartTime() {
+        gameNow.setStartTime(null);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameWithNullFirstTeam() {
+        gameNow.setFirstTeam(null);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameWithNullSecondTeam() {
+        gameNow.setSecondTeam(null);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playCanceledGame() {
+        gameNow.setGameState(GameState.CANCELED);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameWithNotNullFirstTeamScore() {
+        gameNow.setFirstTeamScore(1);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameWithNotNullSecondTeamScore() {
+        gameNow.setSecondTeamScore(1);
+        assertThatThrownBy(() -> gameService.playGame(gameNow)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playGameInFuture() {
+        assertThatThrownBy(() -> gameService.playGame(gameInFuture)).isInstanceOf(IllegalArgumentException.class);
     }
 
     private Game createGame(LocalDateTime startTime, boolean isPlayed, GameState gameState) {
