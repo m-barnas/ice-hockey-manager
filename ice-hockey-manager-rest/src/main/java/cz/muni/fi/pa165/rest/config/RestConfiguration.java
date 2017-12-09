f089c15a83c14177dac64a8ed8bb2dec04e04c7a
Martin Barnáš <xbarnas@fi.muni.cz>
2017-12-09 01:41:27 +0100

    CHG refactor imports, add javadoc

--- ice-hockey-manager-rest/src/main/java/cz/muni/fi/pa165/rest/config/RestConfiguration.java
+++ ice-hockey-manager-rest/src/main/java/cz/muni/fi/pa165/rest/config/RestConfiguration.java
@@ -6,15 +6,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import cz.muni.fi.pa165.rest.interceptors.AllowOriginInterceptor;
 import cz.muni.fi.pa165.service.config.ServiceConfiguration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * @author Martin Barnas 433523@mail.muni.cz
+ */
 @Configuration
 @EnableWebMvc
 @Import(ServiceConfiguration.class)
