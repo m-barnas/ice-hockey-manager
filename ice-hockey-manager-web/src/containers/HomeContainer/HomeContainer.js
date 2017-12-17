b79558aaeb0fb939472103daeeaeec234c6941e0
Martin Barnáš <xbarnas@fi.muni.cz>
2017-12-17 15:27:30 +0100

    NF add antd navigation and layout

---
+++ ice-hockey-manager-web/src/containers/HomeContainer/HomeContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>Home Container</p>
+            </React.Fragment>
+        );
+    }
+}
+
+export default HomeContainer;
