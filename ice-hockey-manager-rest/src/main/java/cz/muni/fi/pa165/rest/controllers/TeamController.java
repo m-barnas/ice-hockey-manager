09c6bb4480f1610956b40101ea6ff7b964f8a146
H269193 <lukas.kotol@cleverlance.com>
2017-12-17 10:52:06 +0100

    NF TeamControllerTest  tests added.

--- ice-hockey-manager-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/TeamController.java
+++ ice-hockey-manager-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/TeamController.java
@@ -7,6 +7,8 @@ import cz.muni.fi.pa165.enums.CompetitionCountry;
 import cz.muni.fi.pa165.exceptions.TeamServiceException;
 import cz.muni.fi.pa165.facade.HumanPlayerFacade;
 import cz.muni.fi.pa165.facade.TeamFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -24,69 +26,81 @@ import static cz.muni.fi.pa165.rest.ApiUri.ROOT_URI_TEAMS;
 @RequestMapping(path = ROOT_URI_TEAMS, produces = MediaType.APPLICATION_JSON_VALUE)
 public class TeamController {
 
+    final static Logger log = LoggerFactory.getLogger(TeamController.class);
+
+
     @Autowired
     TeamFacade teamFacade;
 
-    @Autowired
-    HumanPlayerFacade humanPlayerFacade;
-
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public TeamDto findById(@PathVariable("id") long id) {
+        log.debug("findById({})", id);
         return teamFacade.getTeamById(id);
     }
 
     @RequestMapping(path = "/getByName/{name}", method = RequestMethod.GET)
     public TeamDto findByName(@PathVariable("name") String name) {
+        log.debug("findByName({})", name);
         return teamFacade.findTeamByName(name);
     }
 
     @RequestMapping(path = "/all", method = RequestMethod.GET)
     public List<TeamDto> findAll() {
+        log.debug("findAll");
         return teamFacade.getAllTeams();
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public void deleteById(@PathVariable("id") long id) {
         teamFacade.deleteTeam(id);
+        log.debug("deleteById({})", id);
     }
 
     @RequestMapping(path = "getByCompetitionCountry/{competitionCountry}", method = RequestMethod.GET)
     public List<TeamDto> getByCountry(@PathVariable("competitionCountry") CompetitionCountry competitionCountry) {
+        log.debug("getByCountry({})", competitionCountry);
         return teamFacade.getTeamsByCountry(competitionCountry);
     }
 
     @RequestMapping(path = "/spendMoneyFromBudget", method = RequestMethod.POST)
     public void spendMoneyFromBudget(@RequestBody TeamSpendMoneyDto teamSpendMoneyDto) throws TeamServiceException {
         teamFacade.spendMoneyFromBudget(teamSpendMoneyDto.getTeamId(), teamSpendMoneyDto.getAmount());
+        log.debug("spendMoneyFromBudget({}, {})", teamSpendMoneyDto.getTeamId(), teamSpendMoneyDto.getAmount());
     }
 
     @RequestMapping(path = "/{id}/price", method = RequestMethod.GET)
     public BigDecimal getPrice(@PathVariable("id") long id) {
+        log.debug("getPrice({})", id);
         return teamFacade.getTeamPrice(id);
     }
 
     @RequestMapping(path = "/{id}/attack", method = RequestMethod.GET)
     public int getAttack(@PathVariable("id") long id) {
+        log.debug("getAttack({})", id);
         return teamFacade.getTeamAttackSkill(id);
     }
 
     @RequestMapping(path = "/{id}/defense", method = RequestMethod.GET)
     public int getDefense(@PathVariable("id") long id) {
+        log.debug("getDefense({})", id);
         return teamFacade.getTeamDefenseSkill(id);
     }
 
     @RequestMapping(path = "/create", method = RequestMethod.PUT)
     public void create(@RequestBody TeamDto teamDto) {
         teamFacade.createTeam(teamDto);
+        log.debug("create(createdId = {})", teamDto.getId());
     }
 
     @RequestMapping(path = "/addHockeyPlayer", method = RequestMethod.POST)
     public void addHockeyPlayer(@RequestBody TeamAddRemovePlayerDto teamAddRemovePlayerDto) {
         teamFacade.addHockeyPlayer(teamAddRemovePlayerDto.getTeamId(), teamAddRemovePlayerDto.getHockeyPlayerId());
+        log.debug("addHockeyPlayer(teamId = {}, hockeyPlayerId = {})", teamAddRemovePlayerDto.getTeamId(), teamAddRemovePlayerDto.getHockeyPlayerId());
     }
 
     @RequestMapping(path = "/removeHockeyPlayer", method = RequestMethod.POST)
     public void removeHockeyPlayer(@RequestBody TeamAddRemovePlayerDto teamAddRemovePlayerDto) {
         teamFacade.removeHockeyPlayer(teamAddRemovePlayerDto.getTeamId(), teamAddRemovePlayerDto.getHockeyPlayerId());
+        log.debug("removeHockeyPlayer(teamId = {}, hockeyPlayerId = {})", teamAddRemovePlayerDto.getTeamId(), teamAddRemovePlayerDto.getHockeyPlayerId());
     }
 }
---
+++ ice-hockey-manager-rest/src/test/java/cz/muni/fi/pa165/rest/controllers/TeamControllerTest.java
@@ -0,0 +1,170 @@
+package cz.muni.fi.pa165.rest.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+
+import cz.muni.fi.pa165.dto.HockeyPlayerDto;
+import cz.muni.fi.pa165.dto.TeamDto;
+import cz.muni.fi.pa165.entity.HockeyPlayer;
+import cz.muni.fi.pa165.enums.CompetitionCountry;
+import cz.muni.fi.pa165.enums.Position;
+import cz.muni.fi.pa165.facade.TeamFacade;
+import cz.muni.fi.pa165.service.config.ServiceConfiguration;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.*;
+
+/**
+ * Created by Lukas Kotol on 12/16/2017.
+ */
+@WebAppConfiguration
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class TeamControllerTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private TeamController teamController = new TeamController();
+
+    @Mock
+    private TeamFacade teamFacade;
+
+    @BeforeClass
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = standaloneSetup(teamController).setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
+    }
+
+    @Test
+    public void getAllTeamsTests() throws Exception {
+        doReturn(Collections.unmodifiableList(createTeams()))
+                .when(teamFacade)
+                .getAllTeams();
+
+        mockMvc.perform(get("http://localhost:8080/pa165/ice-hockey-manager/teams/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.[?(@.id==1)].name").value("Team 1"))
+                .andExpect(jsonPath("$.[?(@.id==1)].budget").value(100))
+                .andExpect(jsonPath("$.[?(@.id==2)].name").value("Team 2"));
+    }
+
+    @Test
+    public void findByIdTest() throws Exception {
+        doReturn(createTeams().get(0))
+                .when(teamFacade)
+                .getTeamById(1L);
+
+        mockMvc.perform(get("http://localhost:8080/pa165/ice-hockey-manager/teams/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value("Team 1"))
+                .andExpect(jsonPath("$.budget").value(100))
+                .andExpect(jsonPath("$.competitionCountry").value("CZECH_REPUBLIC"));
+    }
+
+    @Test
+    public void getByNameTest() throws Exception {
+        doReturn(createTeams().get(0))
+                .when(teamFacade)
+                .findTeamByName("Team 1");
+
+        mockMvc.perform(get("http://localhost:8080/pa165/ice-hockey-manager/teams/getByName/Team 1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value("Team 1"))
+                .andExpect(jsonPath("$.budget").value(100))
+                .andExpect(jsonPath("$.competitionCountry").value("CZECH_REPUBLIC"));
+    }
+
+    @Test
+    public void getByCompetitionCountryTest() throws Exception {
+        doReturn(Collections.unmodifiableList(createTeams().subList(0, 1)))
+                .when(teamFacade)
+                .getTeamsByCountry(CompetitionCountry.CZECH_REPUBLIC);
+
+        mockMvc.perform(get("http://localhost:8080/pa165/ice-hockey-manager/teams/getByCompetitionCountry/CZECH_REPUBLIC"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.[?(@.id==1)].name").value("Team 1"))
+                .andExpect(jsonPath("$.[?(@.id==1)].budget").value(100))
+                .andExpect(jsonPath("$.[?(@.id==1)].competitionCountry").value("CZECH_REPUBLIC"));
+    }
+
+//    @Test
+//    public void spendMoneyFromBudgetTest() throws Exception {
+//        doNothing().when(teamFacade).spendMoneyFromBudget(1L, BigDecimal.valueOf(20));
+//
+//        Map<String, String> map = new HashMap<>();
+//
+//        MvcResult result = mockMvc.perform(post("http://localhost:8080/pa165/ice-hockey-manager/teams/spendMoneyFromBudget")
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .param("teamId", String.valueOf(1))
+//                .param("amount", String.valueOf(20))).andReturn();
+//        String res = result.getResponse().getContentAsString();
+//    }
+
+    private List<TeamDto> createTeams() {
+        TeamDto teamDto1 = new TeamDto();
+        teamDto1.setId(1L);
+        teamDto1.setName("Team 1");
+        teamDto1.setBudget(BigDecimal.valueOf(100));
+        teamDto1.setCompetitionCountry(CompetitionCountry.CZECH_REPUBLIC);
+        teamDto1.setHumanPlayerId(1L);
+        teamDto1.setHockeyPlayers(createHockeyPlayers());
+
+        TeamDto teamDto2 = new TeamDto();
+        teamDto2.setId(2L);
+        teamDto2.setName("Team 2");
+        teamDto2.setBudget(BigDecimal.valueOf(500));
+        teamDto2.setCompetitionCountry(CompetitionCountry.ENGLAND);
+        teamDto2.setHumanPlayerId(1L);
+
+        return Arrays.asList(teamDto1, teamDto2);
+
+    }
+
+
+    private Set<HockeyPlayerDto> createHockeyPlayers() {
+        HockeyPlayerDto hockeyPlayerDto = new HockeyPlayerDto();
+        hockeyPlayerDto.setId(1L);
+        hockeyPlayerDto.setName("Jaromir Jagr");
+        hockeyPlayerDto.setAttackSkill(100);
+        hockeyPlayerDto.setDefenseSkill(50);
+        hockeyPlayerDto.setPrice(BigDecimal.valueOf(500));
+        hockeyPlayerDto.setPost(Position.RIGHT_WING);
+        return new HashSet<>(Collections.singletonList(hockeyPlayerDto));
+    }
+}
